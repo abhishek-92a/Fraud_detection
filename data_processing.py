b85fdab35576ca82ba1315c2f839a1bd9313cdc2
abhishek-92a <abhishek.ayyagari92@gmail.com>
2023-12-31 17:02:37 +0530

    added a new function to print a parrern

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,12 @@
 # Data pre - processing 
 
 print("We will perform data process for the fraud detection project in this file")
+
+#sample function to print a pattern
+
+size  =  int(input("Enter the height of pattern: "))
+
+for i in range(0,size,1):
+    for j in range(0,i+1):
+        print(j , end= " ")
+    print(" ")
